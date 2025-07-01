39ef85f3b566448ec4eeae9a8b2cc727adcdfde5
emmy1472 <emmanuelakinmolayan1@gmail.com>
2025-07-01 22:25:30 +0100

    update

--- vms_project/vms_app/urls.py
+++ vms_project/vms_app/urls.py
@@ -67,7 +67,7 @@ urlpatterns = [
 urlpatterns += [
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    #path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
