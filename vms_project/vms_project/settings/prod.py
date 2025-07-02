a25746dda2994981e2741f7cdb0830d72ba45161
emmy1472 <emmanuelakinmolayan1@gmail.com>
2025-07-02 15:34:26 +0100

    up

--- vms_project/vms_project/settings/prod.py
+++ vms_project/vms_project/settings/prod.py
@@ -16,5 +16,13 @@ DATABASES = {
     'default': dj_database_url.config(default='postgresql://vms_kv38_user:y7nVHrF2A7J21GZjkgs1Hbm9sLeaSlcu@dpg-d1dpn5re5dus73dqsimg-a/vms_kv38')
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.sendgrid.net'  # or any other provider
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 
 
