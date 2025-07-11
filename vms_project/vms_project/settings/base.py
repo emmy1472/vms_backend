563cf0a0b98145764fad4577fbe60bdaa28e4f1e
emmy1472 <emmanuelakinmolayan1@gmail.com>
2025-07-11 19:06:53 +0100

    update database

--- vms_project/vms_project/settings/base.py
+++ vms_project/vms_project/settings/base.py
@@ -102,12 +102,7 @@ SIMPLE_JWT = {
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.sendgrid.net'
--- vms_project/vms_project/settings/dev.py
+++ vms_project/vms_project/settings/dev.py
@@ -17,3 +17,10 @@ EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
