b4bf25559584d633cd0740e415e02af674af2513
emmy1472 <emmanuelakinmolayan1@gmail.com>
2025-07-15 00:12:50 +0100

    update device serial to null

--- vms_project/vms_app/models.py
+++ vms_project/vms_app/models.py
@@ -174,7 +174,7 @@ class AccessLog(models.Model):
     person_id = models.PositiveIntegerField()
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     person = GenericForeignKey('content_type', 'person_id')
-    device_serial = models.CharField(max_length=100)
+    device_serial = models.CharField(max_length=100, null=True, blank=True)
     scanned_by = models.ForeignKey('vms_app.User', on_delete=models.SET_NULL, null=True, limit_choices_to={'role': 'security'})
     time_in = models.DateTimeField(auto_now_add=True)
     time_out = models.DateTimeField(null=True, blank=True)
